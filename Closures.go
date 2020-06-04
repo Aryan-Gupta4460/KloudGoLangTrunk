91c3a6b60d638f466ac4886103b80dd92d4b4a7a
Aryan Gupta <62221758+Aryan-Gupta4460@users.noreply.github.com>
2020-06-04 18:13:55 +0530

    My third Commit

--- Closures.go
+++ Closures.go
@@ -4,6 +4,18 @@ import (
 	"fmt"
 )
 
+func retun_annomus_func() func(string) {
+	return func(name string) {
+		fmt.Println(name)
+	}
+}
+func int_seQ() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
+}
 func Say_hello(msg string) {
 	fmt.Println(msg)
 }
@@ -12,5 +24,13 @@ func main() {
 	func(name string) {
 		fmt.Println(name)
 	}("MY name iss Harry")
+	print_func := retun_annomus_func()
+	print_func("Hey we are to close")
 
+	next_int := int_seQ()
+	fmt.Println(next_int())
+	fmt.Println(next_int())
+
+	new_int2 := int_seQ()
+	fmt.Println(new_int2())
 }
