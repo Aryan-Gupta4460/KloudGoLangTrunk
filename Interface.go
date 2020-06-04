55d373ff726f1a0192022cde4c3562ce68085727
Aryan Gupta <62221758+Aryan-Gupta4460@users.noreply.github.com>
2020-06-05 01:31:26 +0530

    My 8 Commmit

---
+++ Interface.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type geometry interface {
+	area() float64
+	perimeter() float64
+}
+type rectangle struct {
+	width, height float64
+}
+type circle struct {
+	radius float64
+}
+
+func (r rectangle) area() float64 {
+	return r.width * r.height
+}
+func (c circle) area() float64 {
+	return math * Pi * c.radius * c.radius
+}
+func (r rectangle) perimeter() float64 {
+	return 2*r.width + 2*r.height
+}
+func (c circle) perimeter() float64 {
+	return 2*math.Pi + c.radius
+}
+func measure(g geometry) {
+	fmt.Println(g)
+	fmt.Println(g.area())
+	fmt.Println(g.perimeter())
+}
+func main() {
+	r := rectangle{width: 3, height: 5}
+	c := circle{radius: 8}
+	measure(r)
+	measure(c)
+
+}
